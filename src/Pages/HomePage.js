257af1a3fbd2c8f99f05c4d01ebe4c89e6e7a3ba
DipanshuTyagi <deepanshutyagi4567@gmail.com>
2023-09-23 14:20:27 +0530

    search bar added

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+
+const SearchBar = () => {
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  };
+
+  return (
+    <div className="grid place-content-center p-10">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center gap-3"
+        action=""
+      >
+        <input
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+          className="border"
+        />
+        <button>
+          <CiSearch className="w-6 h-6" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
