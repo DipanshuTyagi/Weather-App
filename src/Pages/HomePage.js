08a29c31a10e0c2f37eca74364fa87279e0d29bf
DipanshuTyagi <deepanshutyagi4567@gmail.com>
2023-09-24 21:58:11 +0530

    now the req made by search bar is getting the right responce but don't know how to send to the show component

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
-import SearchData from "../api";
+import ShowData from "../components/ShowData";
 
 const HomePage = () => {
   const [searchData, setSearchData] = useState([]);
-
+  useEffect(() => {});
   const handleSearchText = async (term) => {
-    const result = await SearchData(term);
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=0463341a1ad442d9a5d104353232309&q=${term}&aqi=no`
+    );
+    const result = await response.json();
+
+    // const result = await SearchData(term);
 
     setSearchData(result);
     console.log(searchData);
@@ -15,6 +20,8 @@ const HomePage = () => {
   return (
     <div>
       <SearchBar handleSearchText={handleSearchText} />
+
+      <ShowData data={searchData} />
     </div>
   );
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import ShowData from "./ShowData";
 
 const SearchBar = ({ handleSearchText }) => {
+  const apiKey = "eae3475fec784b6b96b93501232409";
   const [text, setText] = useState("");
+  const [wether, setWether] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(text);
     handleSearchText(text);
   };
+  async function fetchData(e) {
+    e.preventDefault();
+    if (text === "") {
+      alert("enter something");
+    } else {
+      const data = fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${text}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
 
+      console.log(data);
+    }
+  }
   return (
     <div className="grid place-content-center p-10">
       <form
@@ -21,10 +37,11 @@ const SearchBar = ({ handleSearchText }) => {
           type="text"
           className="border"
         />
-        <button>
+        <button onClick={fetchData}>
           <CiSearch className="w-6 h-6" />
         </button>
       </form>
+      {wether.data !== undefined ? <ShowData value={wether.data} /> : null}
     </div>
   );
 };
