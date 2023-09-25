17c1e0c0584613f22b6c72e1c64b30776365a862
DipanshuTyagi <deepanshutyagi4567@gmail.com>
2023-09-25 13:01:11 +0530

    nothing spacial

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,14 +6,7 @@ const HomePage = () => {
   const [searchData, setSearchData] = useState([]);
   useEffect(() => {});
   const handleSearchText = async (term) => {
-    const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=0463341a1ad442d9a5d104353232309&q=${term}&aqi=no`
-    );
-    const result = await response.json();
-
-    // const result = await SearchData(term);
-
-    setSearchData(result);
+    await setSearchData(term);
     console.log(searchData);
   };
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
-import ShowData from "./ShowData";
 
 const SearchBar = ({ handleSearchText }) => {
   const apiKey = "eae3475fec784b6b96b93501232409";
   const [text, setText] = useState("");
-  const [wether, setWether] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(text);
-    handleSearchText(text);
   };
   async function fetchData(e) {
     e.preventDefault();
     if (text === "") {
       alert("enter something");
     } else {
-      const data = fetch(
+      const data = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${text}`
       )
         .then((res) => res.json())
         .then((data) => data);
 
-      console.log(data);
+      handleSearchText(data);
     }
   }
   return (
@@ -41,7 +39,6 @@ const SearchBar = ({ handleSearchText }) => {
           <CiSearch className="w-6 h-6" />
         </button>
       </form>
-      {wether.data !== undefined ? <ShowData value={wether.data} /> : null}
     </div>
   );
 };
